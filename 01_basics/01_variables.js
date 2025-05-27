feb335cf5761b1df8c7c2b3c0560240b71df6b48
Anujyogo <anuj.cliffex@gmail.com>
2025-05-27 16:59:34 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const userId = 1
+let userName = "Anuj"
+var userEmail = "anuj@gmail.com"
+userCity = "Faridabad"
+let userSate 
+
+console.log(userId)
+console.table([userId,userName,userEmail,userCity,userSate]);
