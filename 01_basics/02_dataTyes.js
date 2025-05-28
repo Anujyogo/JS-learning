a749f87e54cda6519f892e9b1174263426b40846
Anujyogo <anuj.cliffex@gmail.com>
2025-05-28 17:34:07 +0000

    data types

---
+++ 01_basics/02_dataTyes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all code as newer version
+
+let score = 256
+let score2 = NaN
+let score3 = undefined
+console.log(typeof score);
+
+// few types of data types are listed below
+
+// String = ""
+// Number 
+// Boolean = true/false
+// undifined
+// NaN
+
+console.log(typeof score2)
+console.log(typeof score3)
