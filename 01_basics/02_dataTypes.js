bdbe1180194eb955b48ea03de0e6cb55d80fb0c7
Anujyogo <anuj.cliffex@gmail.com>
2025-05-29 06:16:03 +0000

    conversion of string number boolean

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all code as newer version
+
+let score = 256
+let score2 = NaN
+let score3 = undefined
+let score4 = null
+let score5 = 1
+console.log(typeof score);
+
+// few types of data types are listed below
+
+// String = ""
+// Number 
+// Boolean = true/false
+// undifined
+// NaN
+
+console.log(typeof score2)
+console.log(typeof score3)
+console.log(typeof score4)
+if(score3==true)
+{
+    console.log(score)
+}
+else
+{
+    console.log("something went wrong" + typeof score5 )
+}
+
+// the number 1 itself is true and 0 is false by boolean 
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = true
+let name = 123
+let loggedIn = 1
+console.log(typeof score)
+
+let scoreChangeInNumber = Number(score)
+let nameChangeInString = String(name)
+let loggedInChangeBoolean = Boolean(loggedIn)
+console.log(typeof scoreChangeInNumber)
+console.log(loggedInChangeBoolean)
+console.log(typeof loggedInChangeBoolean)
+console.log(scoreChangeInNumber)
+console.log(typeof nameChangeInString)
